afa4184608a8a23d22cbeda3aa615f5172f79a41
twinaphex <libretro@gmail.com>
2014-05-06 16:12:21 +0200

    (Libretro) Style nits

--- libretro/vkbd.h
+++ libretro/vkbd.h
@@ -1,5 +1,7 @@
 #ifndef VKBD_H
 #define VKBD_H 1
-extern void virtual_kdb(unsigned short int *pixels,int vx,int vy);
-extern int check_vkey2(int x,int y);
+
+void virtual_kdb(unsigned short int *pixels,int vx,int vy);
+int check_vkey2(int x,int y);
+
 #endif
