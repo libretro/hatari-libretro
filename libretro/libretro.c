6bf674a00c8a22314e1639c362c1cc8a01f8b9b9
not6 <r-type@users.noreply.github.com>
2014-11-13 17:32:44 +0100

    fix warning

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -63,8 +63,8 @@ static void update_variables(void)
    {
       char *pch;
       char str[100];
-      snprintf(str, sizeof(str), var.value);
-
+      snprintf(str, sizeof(str), "%s", var.value);
+ 
       pch = strtok(str, "x");
       if (pch)
          retrow = strtoul(pch, NULL, 0);
